2b49b323d89bb3ac9a9ac392499bacd5bc67f501
mdamt <mdamt@adikih>
2010-01-11 11:24:10 +0200

    Removed plurals from classes' names

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,9 +1,9 @@
 from jobs.models import * 
 from django.contrib import admin
 
-admin.site.register(Architectures)
-admin.site.register(Handlers)
-admin.site.register(Jobs)
-admin.site.register(JobTasks)
-admin.site.register(Distributions)
-admin.site.register(HandlerAdministrators)
+admin.site.register(Architecture)
+admin.site.register(Handler)
+admin.site.register(Job)
+admin.site.register(JobTask)
+admin.site.register(Distribution)
+admin.site.register(HandlerAdministrator)
