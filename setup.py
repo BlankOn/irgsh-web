22c5b6efa09594d31c60654d5655e8836e8ac576
Fajran Iman Rusadi <fajran@gmail.com>
2010-12-13 16:18:28 +0100

    Add buildout files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "irgsh-web",
+    version = "0.2",
+    url = 'http://irgsh.blankonlinux.or.id/',
+    description = 'Ir. Robot Gedek, SH',
+    author = 'BlankOn Developers',
+    packages = ['irgsh_web'],
+    install_requires = ['setuptools']
+)
+
