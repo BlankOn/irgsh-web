8ecbfa9982768de7e10cb0e47b093e2d62032ca3
Fajran Iman Rusadi <fajran@gmail.com>
2010-12-13 17:17:40 +0100

    Add the complete packages

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ console_scripts = [
     'irgsh-www = irgsh_web.bin.www:main',
     'irgsh-xmlrpc = irgsh_web.bin.xmlrpc:main',
 ]
+packages = ['irgsh_web']
+packages += ['irgsh_web.%s' % pkg for pkg in find_packages('irgsh_web')]
 
 setup(
     name = "irgsh-web",
@@ -11,7 +13,7 @@ setup(
     url = 'http://irgsh.blankonlinux.or.id/',
     description = 'Ir. Robot Gedek, SH',
     author = 'BlankOn Developers',
-    packages = ['irgsh_web'],
+    packages = packages,
     entry_points = {'console_scripts': console_scripts},
     install_requires = ['setuptools', 'django-openid-auth', 'django-xmlrpc',
                         'python-openid']
