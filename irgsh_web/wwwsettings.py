a75b0654f3a3d6c39e90a53cd9e4a259237650b5
Fajran Iman Rusadi <fajran@gmail.com>
2010-12-19 10:06:48 +0100

    Update celery settings

--- irgsh_web/wwwsettings.py
+++ irgsh_web/wwwsettings.py
@@ -6,3 +6,19 @@ XMLRPC_METHODS = ()
 import djcelery
 djcelery.setup_loader()
 
+CELERY_QUEUES = {
+    'celery': {
+        'exchange': 'celery',
+        'exchange_type': 'direct',
+    },
+}
+CELERY_DEFAULT_QUEUE = 'celery'
+CELERY_DEFAULT_EXCHANGE = 'celery'
+CELERY_DEFAULT_EXCHANGE_TYPE = 'direct'
+
+BROKER_HOST = '192.168.56.180'
+BROKER_PORT = 5672
+BROKER_USER = 'irgsh'
+BROKER_PASSWORD = 'irgsh'
+BROKER_VHOST = 'irgsh'
+
