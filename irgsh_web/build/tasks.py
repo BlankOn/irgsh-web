d17edde0347bb6e430bf363fe03b5318dc787f45
Fajran Iman Rusadi <fajran@gmail.com>
2010-12-31 15:40:36 +0100

    Refactor task initialization info a separate class

--- irgsh_web/build/tasks.py
+++ irgsh_web/build/tasks.py
@@ -15,43 +15,6 @@ class InitSpecification(Task):
     ignore_result = True
 
     def run(self, spec_id):
-        spec = Specification.objects.get(pk=spec_id)
-
-        task_name = BuildPackage.name # 'irgsh_node.tasks.BuildPackage'
-        args = utils.create_build_task_param(spec)
-        kwargs = None
-
-        archs = Architecture.objects.filter(active=True)
-        for arch in archs:
-            task_id = utils.build_task_id()
-
-            # store task info
-            task = BuildTask()
-            task.task_id = task_id
-            task.specification = spec
-            task.architecture = arch
-            task.save()
-
-            # declare exchange, queue, and binding
-            routing_key = 'builder.%s' % arch.name
-
-            consumer = self.get_consumer()
-            consumer.queue = 'builder_%s' % arch.name
-            consumer.exchange = 'builder'
-            consumer.exchange_type = 'topic'
-            consumer.routing_key = routing_key
-            consumer.declare()
-            consumer.connection.close()
-
-            # create build package task
-            opts = {'exchange': 'builder',
-                    'exchange_type': 'topic',
-                    'routing_key': routing_key,
-                    'task_id': task_id}
-
-            # execute build task asynchronously
-            s = subtask(task_name, args, kwargs, opts)
-            s.apply_async()
-
-        spec.save()
+        init = utils.SpecInit(spec_id)
+        init.start()
 
--- irgsh_web/build/utils.py
+++ irgsh_web/build/utils.py
@@ -80,3 +80,347 @@ def build_source_opts(source_type, source_opts):
 
         raise ValueError(_('Invalid source options for Bazaar'))
 
+class SpecInit(object):
+    '''
+    Prepare and distribute specification
+    '''
+
+    def __init__(self, spec_id):
+        from .models import Specification
+
+        self.spec_id = spec_id
+        self.spec = Specification.objects.get(pk=spec_id)
+
+        self.description_sent = False
+        self.distributed = False
+
+        self.source_name = None
+        self.orig_name = None
+
+    def start(self):
+        init_status = spec.status
+
+        try:
+            self.init()
+            self.source_name = self.download_source()
+            self.inspect_source()
+            self.orig_name = self.download_orig()
+            self.distribute()
+        except (ValueError, AssertionError), e:
+            if init_status != spec.status:
+                # Status has not been changed, set to failed
+                spec.status = -1
+                spec.save()
+
+    def init(self):
+        self.target = os.path.join(settings.DOWNLOAD_TARGET, int(self.spec_id))
+        if not os.path.exists(self.target):
+            os.makedirs(self.target)
+
+    def download_source(self):
+        '''
+        Download source file according to the type
+        '''
+        from .models import SOURCE_TYPE
+        types = zip(*SOURCE_TYPE)[0]
+
+        assert self.spec.source_type in types
+
+        func_name = '_download_source_%s' % self.spec.source_type
+        assert hasattr(self, func_name)
+
+        func = getattr(self, func_name)
+        func()
+
+    def update_resource(self, **param):
+        '''
+        Update resource database associated to the specification
+        '''
+        from .models import SpecificationResource
+
+        SpecificationResource.objects.get_or_create(specification=self.spec)
+        SpecificationResource.objects.filter(specification=self.spec) \
+                                     .update(**param)
+
+    def _download_source_bzr(self):
+        '''
+        Download source from a Bazaar repository
+        '''
+        self.update_resource(source_started=datetime.now())
+
+        try:
+            tmpdir = tempfile.mkdtemp()
+
+            # Get options
+            source = self.spec.source
+            opts = self.spec.source_opts
+            if opts is None:
+                opts = {}
+
+            # Open branch
+            branch = Branch.open(self.spec.source)
+
+            # Get revision
+            revid = None
+            if 'revision' in opts:
+                revid = branch.get_rev_id(opts['revision'])
+            elif 'tag' in opts:
+                revid = branch.tags.lookup_tag(opts['tag'])
+            else:
+                revid = branch.last_revision()
+
+            # Get tree
+            tree = branch.repository.revision_tree(revid)
+
+            # Find debian/control and debian/changelog
+            #   These two files are checked before extracting the whole files
+            #   because they will determine whether this specification is
+            #   allowed to proceed or not
+            tree.lock_read()
+            if not tree.has_filename('debian/control') or \
+               not tree.has_filename('debian/changelog'):
+                raise ValueError('Invalid source')
+
+            file_id_control = None
+            file_id_changelog = None
+            for item in tree.list_files(from_dir='debian', recursive=False):
+                if item[2] == 'file':
+                    if item[0] == 'control':
+                        file_id_control = item[3]
+                    elif item[0] == 'changelog':
+                        file_id_changelog = item[3]
+
+            fcontrol = os.path.join(tmpdir, 'control')
+            fchangelog = os.path.join(tmpdir, 'changelog')
+
+            f = open(fcontrol, 'wb')
+            f.write(tree.get_file(file_id_control).read())
+            f.close()
+
+            f = open(fchangelog, 'wb')
+            f.write(tree.get_file(file_id_changelog).read())
+            f.close()
+
+            tree.unlock()
+
+            # Get source package name
+            changelog = Changelog(fchangelog)
+            name = changelog.package
+            version = str(changelog.version).split(':')[-1]
+
+            # Send changelog and control
+            #   send_description will raise an exception if this specification
+            #   is rejected
+            self.send_description(fchangelog, fcontrol)
+
+            # Package is accepted, distribution can begin
+            self.distribute()
+
+            # Export
+            #   everything looks good, continue to proceed
+            source_name = '%s_%s.tar.gz' % (name, version)
+            self.update_resource(source_name=source_name)
+
+            target = os.path.join(self.target, source_name)
+            export(tree, target)
+
+            self.update_resource(source_finished=datetime.now())
+
+            return source_name
+
+        finally:
+            shutil.rmtree(tmpdir)
+
+    def _download_source_tarball(self):
+        '''
+        Download source file in an archive
+        '''
+        self.update_resource(source_started=datetime.now())
+
+        try:
+            source_name = os.path.basename(self.spec.source)
+            self.update_resource(source_name=source_name)
+
+            fname, http = urllib.urlretrieve(self.spec.source)
+            target = os.path.join(self.target, source_name)
+            shutil.move(fname, target)
+
+            self.update_resource(source_finished=datetime.now())
+
+            return source_name
+
+        finally:
+            if os.path.exists(fname):
+                os.unlink(fname)
+
+    def send_description(self, changelog, control):
+        '''
+        Send package description to the manager
+
+        Depending on the response, this will determine whether
+        this specification is allowed to proceed or not.
+        '''
+        from . import manager
+
+        if self.description_sent:
+            return
+
+        try:
+            tmpdir = tempfile.mkdtemp()
+            gzchangelog = os.path.join(tmpdir, 'changelog.gz')
+            gzcontrol = os.path.join(tmpdir, 'control.gz')
+
+            gz = gzip.GzipFile(gzchangelog, 'wb')
+            if hasattr(changelog, 'read'):
+                gz.write(changelog.read())
+            else:
+                gz.write(open(changelog, 'rb').read())
+            gz.close()
+
+            gz = gzip.GzipFile(gzcontrol, 'wb')
+            if hasattr(control, 'read'):
+                gz.write(control.read())
+            else:
+                gz.write(open(control, 'rb').read())
+            gz.close()
+
+            res = manager.send_description(self.spec.id,
+                                           gzchangelog, gzcontrol)
+            if res['status'] != 'ok':
+                # Package is rejected
+                raise ValueError(_('Package rejected: %(msg)s') % \
+                                 {'msg': res['msg']})
+
+        finally:
+            self.description_sent = True
+            shutil.rmtree(tmpdir)
+
+    def inspect_source(self):
+        '''
+        Extract source and send description
+        '''
+        # FIXME is it safe to assume that all tarball source files
+        #       are in tar/gz format?
+        tar = tarfile.open(self.source_name, 'r:gz')
+        fchangelog = tar.extractfile('debian/changelog')
+        fcontrol = tar.extractfile('debian/control')
+
+        self.send_description(fchangelog, fcontrol)
+
+    def download_orig(self):
+        '''
+        Download orig file
+        '''
+        if self.spec.orig is None:
+            return None
+
+        self.update_resource(orig_started=datetime.now())
+
+        try:
+            orig_name = os.path.basename(self.spec.orig)
+            self.update_resource(orig_name=orig_name)
+
+            fname, http = urllib.urlretrieve(self.spec.orig)
+            target = os.path.join(self.target, orig_name)
+            shutil.move(fname, target)
+
+            self.update_resource(orig_finished=datetime.now())
+
+            return orig_name
+
+        finally:
+            if os.path.exists(fname):
+                os.unlink(fname)
+
+    def declare_queues(self, archs):
+        '''
+        Declare queues, exchanges, and routing keys to the builders
+        '''
+        for arch in archs:
+            # declare exchange, queue, and binding
+            routing_key = 'builder.%s' % arch.name
+            consumer = self.get_consumer()
+            consumer.queue = 'builder_%s' % arch.name
+            consumer.exchange = 'builder'
+            consumer.exchange_type = 'topic'
+            consumer.routing_key = routing_key
+            consumer.declare()
+            consumer.connection.close()
+
+    def get_archs(self, spec):
+        '''
+        List all architectures associated to this specification
+        '''
+        return spec.distribution.repo.architectures.all()
+
+    def distribute(self):
+        '''
+        Distribute specification to builders
+        '''
+        from celery.task.sets import subtask
+
+        from .models import BuildTask
+        from irgsh_node.tasks import BuildPackage
+        from irgsh.specification import Specification as BuildSpecification
+        from irgsh.distribution import Distribution as BuildDistribution
+
+        if self.distributed:
+            return
+
+        # Declare queues
+        archs = self.get_archs(spec)
+        self.declare_queues(archs)
+
+        # Prepare arguments
+        task_name = BuildPackage.name
+        args = utils.create_build_task_param(spec)
+
+        build_spec = BuildSpecification(spec.source, spec.orig,
+                                        spec.source_type, spec.source_opts)
+
+        dist = spec.distribution
+        build_dist = BuildDistribution(dist.name, dist.mirror, dist.dist,
+                                       dist.components, dist.extra)
+
+        args = [spec.id, build_spec, build_dist]
+        kwargs = None
+
+        spec_id, d, s = args
+        print 'init: spec_id=%s' % spec_id
+        print '- distribution: name=%s mirror=%s dist=%s comp=%s extra=%s' % (d.name, d.mirror, d.dist, repr(d.components), repr(d.extra))
+        print '- spec: location=%s type=%s orig=%s opts=%s' % (s.location, repr(s.source_type), repr(s.orig), repr(s.source_opts))
+
+        # Distribute to builder of each architecture
+        subtasks = []
+        for arch in archs:
+            task_id = utils.build_task_id()
+
+            # store task info
+            task = BuildTask()
+            task.task_id = task_id
+            task.specification = spec
+            task.architecture = arch
+            task.save()
+
+            # declare exchange, queue, and binding
+            routing_key = 'builder.%s' % arch.name
+
+            # create build package task
+            opts = {'exchange': 'builder',
+                    'exchange_type': 'topic',
+                    'routing_key': routing_key,
+                    'task_id': task_id}
+
+            # execute build task asynchronously
+            s = subtask(task_name, args, kwargs, opts)
+            subtasks.append(s)
+
+        spec.status = 3
+        spec.save()
+
+        for s in subtasks:
+            print '  - subtask: %s' % s
+            s.apply_async()
+
+        self.distributed = True
+
