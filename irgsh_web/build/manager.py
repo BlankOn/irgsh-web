9a8a62fadccd769a20a9bd29997f53e80eeb265d
Fajran Iman Rusadi <fajran@gmail.com>
2011-01-18 11:19:47 +0100

    Remove unused code

--- irgsh_web/build/manager.py
+++ irgsh_web/build/manager.py
@@ -5,14 +5,6 @@ from django.core.urlresolvers import reverse
 
 from irgsh_web import utils
 
-def send_control(task_id, fname):
-    host = settings.SERVER.rstrip('/')
-    url = URL_CONTROL % {'host': host, 'task_id': task_id}
-
-    param = {'control': open(fname, 'rb'),
-             'builder': settings.NODE_NAME}
-    utils.send_message(url, param)
-
 def send_spec_description(spec_id, changelog, control):
     host = settings.SERVER.rstrip('/')
     path = reverse('build_spec_description', args=[str(spec_id)])
