6153052590262c24a2c85c7c135aeb6d32cc6f5f
Fajran Iman Rusadi <fajran@gmail.com>
2011-02-24 23:01:29 +0100

    Update database configuration

--- irgsh_web/settings.py
+++ irgsh_web/settings.py
@@ -11,12 +11,12 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'web-builder.db'             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASES = {
+    'default': {
+        'ENGINE': 'sqlite3',
+        'NAME': 'irgsh.db',
+    }
+}
 
 DEFAULT_FROM_EMAIL = 'webmaster@blankon.in'
 EMAIL_USE_TLS = False
