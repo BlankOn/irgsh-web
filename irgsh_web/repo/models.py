b45e2efc874a108f56d11ac6744a63f160571e0a
Fajran Iman Rusadi <fajran@gmail.com>
2011-03-23 08:07:30 +0100

    Update Package and its relations

--- irgsh_web/repo/models.py
+++ irgsh_web/repo/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import ugettext as _
+from django.core.exceptions import ValidationError
 
 class Architecture(models.Model):
     '''
@@ -47,14 +48,26 @@ class Package(models.Model):
     '''
     List of (source) packages
     '''
-    name = models.CharField(max_length=1024)
+    name = models.CharField(max_length=1024, unique=True)
+    distribution = models.ManyToManyField(Distribution, through='PackageDistribution')
+
+    def __unicode__(self):
+        return self.name
+
+class PackageDistribution(models.Model):
+    package = models.ForeignKey(Package)
     distribution = models.ForeignKey(Distribution)
     component = models.ForeignKey(Component)
 
     class Meta:
-        unique_together = ('name', 'distribution',)
-        ordering = ('component__distribution__name', 'component__name', 'name',)
+        unique_together = ('package', 'distribution',)
+
+    def clean(self):
+        relation = self.distribution.components.filter(pk=self.component.id)
+        if len(relation) == 0:
+            raise ValidationError('Distribution %s does not have %s component' % \
+                                  (self.distribution, self.component))
 
     def __unicode__(self):
-        return '%s/%s/%s' % (self.name, self.component.distribution.name, self.component.name)
+        return '%s (%s)' % (self.distribution, self.component)
 
