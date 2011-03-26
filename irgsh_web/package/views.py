96d1f9f5d049ba13c873c1449318f7fa1c18c55a
Fajran Iman Rusadi <fajran@gmail.com>
2011-03-27 00:38:59 +0100

    Package information

--- irgsh_web/package/views.py
+++ irgsh_web/package/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 
-from irgsh_web.repo.models import Package
+from irgsh_web.repo.models import Package, PackageDistribution
 from irgsh_web.build.models import Specification
 from irgsh_web.utils import paginate
 
@@ -14,7 +14,31 @@ def _package_required(func):
 
 @_package_required
 def show(request, package):
-    context = {'package': package}
+    # Published packages
+    dists = PackageDistribution.objects.filter(package=package) \
+                                       .values_list('distribution', flat=True)
+
+    published = []
+    for dist in dists:
+        try:
+            spec = Specification.objects.filter(package=package,
+                                                distribution__repo=dist,
+                                                status=999) \
+                                        .order_by('-finished') \
+                                        .select_related()[0]
+            published.append(spec)
+        except IndexError:
+            pass
+
+    # Recent builds
+
+    builds = Specification.objects.filter(package=package) \
+                                  .order_by('-finished') \
+                                  .select_related()[:15]
+
+    context = {'package': package,
+               'published': published,
+               'builds': builds}
     return render_to_response('package/show.html', context,
                               context_instance=RequestContext(request))
 
