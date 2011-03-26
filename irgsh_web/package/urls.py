2801985b151a2c0789291f7bbbe4e75f31891cf6
Fajran Iman Rusadi <fajran@gmail.com>
2011-03-26 02:31:31 +0100

    Initial version of package information page

--- irgsh_web/package/urls.py
+++ irgsh_web/package/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('irgsh_web.package.views',
+    url(r'^(?P<package_name>[a-z0-9.-]+)/$',
+        'show', name='package_show'),
     url(r'^$', 'index', name='package_index'),
 )
 
--- irgsh_web/package/views.py
+++ irgsh_web/package/views.py
@@ -2,8 +2,21 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 
+from irgsh_web.repo.models import Package
 from irgsh_web.build.models import Specification
 from irgsh_web.utils import paginate
+
+def _package_required(func):
+    def _func(request, package_name, *args, **kwargs):
+        package = get_object_or_404(Package, name=package_name)
+        return func(request, package, *args, **kwargs)
+    return _func
+
+@_package_required
+def show(request, package):
+    context = {'package': package}
+    return render_to_response('package/show.html', context,
+                              context_instance=RequestContext(request))
 
 def index(request):
     build_list = Specification.objects.filter(status=999) \
