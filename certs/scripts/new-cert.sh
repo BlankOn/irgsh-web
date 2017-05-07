809b41a8d0772e87ea9bb470d01f0b1c834852ec
Herpiko Dwi Aguno <herpiko@gmail.com>
2017-05-07 08:59:42 +0000

    Minor fixes

--- certs/scripts/new-cert.sh
+++ certs/scripts/new-cert.sh
@@ -5,7 +5,7 @@ if [ "$1" == "" ]; then
 fi
 
 #export 
-env OPENSSL_CONF=konfigurasi/irgsh-openssl.cnf  SSLEAY_CONFIG="-subj \"/C=ID/ST=DKI Jakarta/L=Jakarta/O=BlankOn Linux/OU=Infrastruktur/CN=$1\"" ./scripts/ca.pl -newreq-nodes 
+env OPENSSL_CONF=conf/irgsh-openssl.cnf  SSLEAY_CONFIG="-subj \"/C=ID/ST=DKI Jakarta/L=Jakarta/O=BlankOn Linux/OU=Infrastruktur/CN=$1\"" ./scripts/ca.pl -newreq-nodes 
 ./scripts/ca.pl -sign
 mkdir $1
 mv new*.pem $1/
--- certs/scripts/renew-ssl.sh
+++ certs/scripts/renew-ssl.sh
@@ -5,11 +5,11 @@ if [ "$1" == "" ]; then
     exit 1
 fi
 
-export OPENSSL_CONF=konfigurasi/irgsh-openssl.cnf
+export OPENSSL_CONF=conf/irgsh-openssl.cnf
 if [ ! -d expired.`date +%Y` ]; then mkdir expired.`date +%Y`; fi
 mv $1/ expired.`date +%Y`/
 echo $1
-env SSLEAY_CONFIG="-subj \"/C=ID/ST=DKI Jakarta/L=Jakarta/O=BlankOn Linux/OU=Infrastruktur/CN=$1\"" ./CA.pl -newreq-nodes 
-./CA.pl -sign
+env SSLEAY_CONFIG="-subj \"/C=ID/ST=DKI Jakarta/L=Jakarta/O=BlankOn Linux/OU=Infrastruktur/CN=$1\"" ./scripts/ca.pl -newreq-nodes 
+./scripts/ca.pl -sign
 mkdir $1
 mv new*.pem $1/
